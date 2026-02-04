056792186d355248c9077c8dca666a50e618b20c
Soumya Ray <soumya.ray@gmail.com>
2026-02-05 05:08:13 +0800

    Update sideways to v0.2.1

--- Formula/sideways.rb
+++ Formula/sideways.rb
@@ -1,8 +1,8 @@
 class Sideways < Formula
   desc "Git worktree helper - easily manage parallel working directories"
   homepage "https://github.com/soumyaray/sideways"
-  url "https://github.com/soumyaray/sideways/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "2e65fec4507c2971492473f9b58276d19a83a18dfb3c3affa7c876030a153c67"
+  url "https://github.com/soumyaray/sideways/archive/refs/tags/v0.2.1.tar.gz"
+  sha256 "118c3f03bf5b6fef27e1b0b243b5c098f84d5e48a29f90b994de056384e81c76"
   license "MIT"
   head "https://github.com/soumyaray/sideways.git", branch: "main"
 
